babdd53306b90aca4c5a45e98619c54c4cd66c77
Maksym Heraskevych <heraskevych.maksym@gmail.com>
2019-02-11 13:59:14 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,31 @@
+$(document).ready(function(){
+
+	$('.next') .click(function(){
+		var currentImage =$ ('.img.house');
+		var currentImageIndex =$ ('.img.house') .index();
+		var nextImageIndex = currentImageIndex + 1;
+		var nextImage = $ ('.img').eq(nextImageIndex);
+		currentImage.fadeOut(1000);
+		currentImage.removeClass('house')
+
+		if(nextImageIndex == ($('.img:last') .index()+1)){
+			$('.img').eq(0).fadeIn(1000);
+			$('.img').eq(0).addClass('house');
+		} else{
+			nextImage.fadeIn(1000);
+			nextImage.addClass('house');
+		}
+
+	});
+	$('.back') .click(function(){
+		var currentImage =$ ('.img.house');
+		var currentImageIndex =$ ('.img.house') .index();
+		var prevImageIndex = currentImageIndex - 1;
+		var prevImage = $ ('.img').eq(prevImageIndex);
+
+		currentImage.fadeOut(1000);
+		currentImage.removeClass('house');
+		prevImage.fadeIn(1000);
+		prevImage.addClass('house');
+	});
+});
